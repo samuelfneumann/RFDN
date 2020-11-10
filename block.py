2eacb1e97241a662aea0d9a8e976fc09334f692b
Samuel Neumann <samuelfneumann@gmail.com>
2020-11-10 15:23:10 -0700

    Add new RFDN networks
    Added two new RFDN networks for the ablation study. One RFDN network as
    RFDB's of size 2 layers, and the other has RFDB's of size 1 layers. The
    original RFDB's have size 3 layers.

--- block.py
+++ block.py
@@ -130,7 +130,7 @@ class ESA(nn.Module):
 
 
 class RFDB(nn.Module):
-    def __init__(self, in_channels, distillation_rate=0.25):
+    def __init__(self, in_channels):
         super(RFDB, self).__init__()
         self.dc = self.distilled_channels = in_channels//2
         self.rc = self.remaining_channels = in_channels
@@ -167,7 +167,7 @@ class RFDB(nn.Module):
 
 
 class RFDB2(nn.Module):
-    def __init__(self, in_channels, distillation_rate=0.25):
+    def __init__(self, in_channels):
         super(RFDB2, self).__init__()
         self.dc = self.distilled_channels = in_channels//2
         self.rc = self.remaining_channels = in_channels
@@ -178,7 +178,7 @@ class RFDB2(nn.Module):
         self.c3 = conv_layer(self.remaining_channels, self.dc, 3)
 
         self.act = activation('lrelu', neg_slope=0.05)
-        self.c5 = conv_layer(self.dc*4, in_channels, 1)
+        self.c4 = conv_layer(self.dc*3, in_channels, 1)
         self.esa = ESA(in_channels, nn.Conv2d)
 
     def forward(self, input):
@@ -193,10 +193,35 @@ class RFDB2(nn.Module):
         r_c3 = self.act(self.c3(r_c2))
 
         out = torch.cat([distilled_c1, distilled_c2, r_c3], dim=1)
-        out_fused = self.esa(self.c5(out))
+        out_fused = self.esa(self.c4(out))
 
         return out_fused
 
+
+class RFDB1(nn.Module):
+    def __init__(self, in_channels):
+        super(RFDB1, self).__init__()
+        self.dc = self.distilled_channels = in_channels//2
+        self.rc = self.remaining_channels = in_channels
+        self.c1_d = conv_layer(in_channels, self.dc, 1)
+        self.c1_r = conv_layer(in_channels, self.rc, 3)
+        self.c2 = conv_layer(self.remaining_channels, self.dc, 3)
+
+        self.act = activation('lrelu', neg_slope=0.05)
+        self.c3 = conv_layer(self.dc*2, in_channels, 1)
+        self.esa = ESA(in_channels, nn.Conv2d)
+
+    def forward(self, input):
+        distilled_c1 = self.act(self.c1_d(input))
+        r_c1 = (self.c1_r(input))
+        r_c1 = self.act(r_c1+input)
+
+        r_c2 = self.act(self.c2(r_c1))
+
+        out = torch.cat([distilled_c1, r_c2], dim=1)
+        out_fused = self.esa(self.c3(out))
+
+        return out_fused
 
 
 def pixelshuffle_block(in_channels, out_channels, upscale_factor=2, kernel_size=3, stride=1):
